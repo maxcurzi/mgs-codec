3a89996aacf83ebacd410b252103b655af1590ce
Max Curzi <massimiliano.curzi@gmail.com>
2023-03-25 19:01:43 +0000

    initial prototype style

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# mgs-codec
-Metal Gear Solid Codec communication app for end-to-end encrypted p2p video communication
+# Tauri + Vanilla
+
+This template should help get you started developing with Tauri in vanilla HTML, CSS and Javascript.
+
+## Recommended IDE Setup
+
+- [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
---
+++ src/main.js
@@ -0,0 +1,26 @@
+const { invoke } = window.__TAURI__.tauri;
+
+let greetInputEl;
+let greetMsgEl;
+
+async function greet() {
+  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  greetInputEl = document.querySelector("#greet-input");
+  greetMsgEl = document.querySelector("#greet-msg");
+  document
+    .querySelector("#greet-button")
+    .addEventListener("click", () => greet());
+});
+
+var codec = document.getElementById("codec");
+var codecText = document.getElementById("codec-text");
+var codecButton = document.getElementById("codec-button");
+
+codecButton.addEventListener("click", function () {
+  var message = codecText.value;
+  console.log(message);
+});
